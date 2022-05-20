a9dc0a6f6fc761ee87e6f56fbd98fde6762843d4
MandinaVrn <95045406+MandinaVrn@users.noreply.github.com>
2022-05-20 17:18:00 +0200

    Initial commit

---
+++ film.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Movie DataBase</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/png" sizes="16×6" href="image/logo28.png">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="contenu_body">
+            <header>
+                <div class="titre_logo">
+                    <a href="index.html"><span class="logo"><img class="logo" src="image/mdb.png" alt="logo du site"></a></span> <!-- logo -->
+                    <h3>Movie DB</h3><!--ajouter logo ?? -->
+                </div> <!-- fin titre + logo-->
+                <!-- ajouter barre de recherche-->
+            </header>
+            <div class="contenu">
+
+                
+                <?php
+                    try
+                    {
+                        // On se connecte à MySQL
+                        $mysqlClient = new PDO('mysql:host=localhost;dbname=film_db;charset=utf8', 'root', 'root');
+                    }
+                    catch(Exception $e)
+                    {
+                        // En cas d'erreur, on affiche un message et on arrête tout
+                            die('Erreur : '.$e->getMessage());
+                    }
+
+                    // Si tout va bien, on peut continuer
+
+                    // On récupère tout le contenu de la table recipes
+                    $sqlQuery = 'SELECT * FROM film where id_film = 1';
+                    $filmsStatement = $mysqlClient->prepare($sqlQuery);
+                    $filmsStatement->execute();
+                    $films = $filmsStatement->fetchAll();
+
+                    // On affiche chaque recette une à une
+                    foreach ($films as $film) {
+                    ?>
+                        
+                    <?php
+                    }
+                ?>
+              
+
+                <div class="bloc_infos">
+                    <span class="poster"><a href="<?php echo $film['poster']; ?>"><img src="<?php echo $film['poster']; ?>" alt="poster...."></a> <!-- poster --></span>
+                    <div class="bloc_infos_e"> <!-- bloc infos écrites (bloc infos sans poster ) -->
+                        <h3 class="titre_film"><?php echo $film['titre']; ?></h3> <!-- Titre du film -->
+                        <p class="date"><?php echo $film['annee']; ?></p> <!-- date de sortie en france du film -->
+                        <p class="synopsis"><?php echo $film['synopsis']; ?></p> <!-- synopsis du film -->
+                    </div> <!-- fin bloc infos écrites -->
+                </div> <!-- fin bloc infos -->
+                <div class="bloc_casting">
+                    <div class="ensemble_photo">   
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                        <div class="photo_acteur"><a href="google.com/search?q=acteur"><img src="image/casting/acteur.jpg"><span class="legende"></a><h5 class="nom_acteur">Prénom Nom</h5><h5 class="nom_perso">Prénom Nom</h5></div>
+                    </div> <!-- fin ensemble photo -->
+                </div>
+                <div class="bloc_trailer">
+                    <!-- insérer intégration player youtube du trailer du fillm -->
+                    
+                </div>
+
+                
+
+
+                
+            
+
+            </div> <!-- fin contenu -->
+        </div> <!-- fin contenu body -->
+
+       
+
+
+    </body>
+</html>
---
+++ table_film.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: May 19, 2022 at 11:17 AM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `film_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film`
+--
+
+CREATE TABLE `film` (
+  `id_film` int(11) NOT NULL,
+  `titre` text NOT NULL,
+  `annee` year(4) NOT NULL,
+  `poster` varchar(2000) NOT NULL,
+  `synopsis` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `trailer` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `film`
+--
+
+INSERT INTO `film` (`id_film`, `titre`, `annee`, `poster`, `synopsis`, `trailer`) VALUES
+(1, 'The Batman', 2022, '', 'Durant les deux années passées à arpenter les rues en tant que Batman, Bruce Wayne s\'\'est aventuré au cœur des ténèbres de Gotham City. Disposant de seulement quelques alliés de confiance au sein du monde corrompu qu\'\'est l\'\'élite de la ville, le justicier solitaire s\'\'est imposé comme l\'\'unique incarnation de la vengeance parmi ses concitoyens. Lorsqu\'\'un meurtrier s\'\'en prend aux plus grandes personnalités de Gotham à l\'\'aide de machinations sadiques, le plus grand détective du monde se lance dans une enquête dans la pègre en suivant des indices mystérieux, croisant ainsi Selina Kyle, alias Catwoman, Oswald Cobblepot, alias le Pingouin, Carmine Falcone et Edward Nashton, alias le Sphinx. Alors que les pistes et les plans du criminel s\'\'éclaircissent, Batman doit tisser de nouveaux liens, démasquer le coupable et rétablir la justice à Gotham City, depuis trop longtemps empoisonnée par l\'\'abus de pouvoir et la corruption.\r\n', ''),
+(2, 'L\'Âge de glace', 2002, '', 'Vingt mille ans avant notre ère, lorsque Scrat, un rongeur obstiné, fend la banquise et déclenche un nouvel Âge de Glace, une vaste cohorte de mammifères s’assemble et commence à émigrer vers le sud. Manny, un mammouth solitaire qui n’en fait qu’à sa tête, choisit d’aller vers le nord et se voit bientôt rejoint par Sid, un petit paresseux volubile en quête de protecteur. Ce dernier l’oblige bientôt à recueillir un bébé humain, Roshan, que sa mère leur a confié avant de mourir, et à ramener celui‐ci à son père, chasseur nomade parti vers le nord. Sur leur chemin, les deux compères devront protéger Roshan de Diego, un tigre aux dents de sabre chargé par son chef Soto de capturer le bébé.', ''),
+(3, 'Wonder Woman', 2017, '', 'Avant d ` `être Wonder Woman, elle s ` `appelait Diana, princesse des Amazones, entraînée pour être une guerrière impossible à conquérir. Elle est élevée sur une île isolée et paradisiaque, mais lorsqu ` `un pilote américain s ` `écrase sur leur rivage et annonce qu ` `un conflit à grande échelle fait rage dans le monde, Diana quitte son foyer, convaincue qu ` `elle doit arrêter cette menace. Combattant aux côtés de cet homme et des siens pour mettre fin à cette guerre et à toutes les guerres, Diana découvre ses vrais pouvoirs... Et son véritable destin.\r\n', ''),
+(4, 'Ratatouille', 2007, '', 'pton, 10 avril 1912. Le paquebot le plus grand et le plus moderne du monde, réputé pour son insubmersibilité, le « Titanic », appareille pour son premier voyage. Quatre jours plus tard, il heurte un iceberg. À son bord, un artiste pauvre et une grande bourgeoise tombent amoureux.', ''),
+(5, 'Eternal Sunshine of the Spotless Mind', 2004, '', 'Joël et Clémentine ne voient plus que les mauvais côtés de leur tumultueuse histoire d’amour, au point que celle‐ci fait effacer de sa mémoire toute trace de cette relation. Effondré, Joël contacte l’inventeur du procédé Lacuna, le Dr. Mierzwiak, pour qu’il extirpe également de sa mémoire tout ce qui le rattachait à Clémentine. Deux techniciens, Stan et Patrick, s’installent à son domicile et se mettent à l’œuvre, en présence de la secrétaire, Mary. Les souvenirs commencent à défiler dans la tête de Joël, des plus récents aux plus anciens, et s’envolent un à un, à jamais. Mais en remontant le fil du temps, Joël redécouvre ce qu’il aimait depuis toujours en Clémentine – l’inaltérable magie d’un amour dont rien au monde ne devrait le priver. Luttant de toutes ses forces pour préserver ce trésor, il engage alors une bataille de la dernière chance contre Lacuna…', ''),
+(6, 'Titanic', 1998, '', 'Southampton, 10 avril 1912. Le paquebot le plus grand et le plus moderne du monde, réputé pour son insubmersibilité, le « Titanic », appareille pour son premier voyage. Quatre jours plus tard, il heurte un iceberg. À son bord, un artiste pauvre et une grande bourgeoise tombent amoureux.', ''),
+(7, 'Ready Player One', 2018, '', 'En 2045, le monde est au bord du chaos. Les êtres humains se réfugient dans l\'OASIS, univers virtuel mis au point par le brillant et excentrique James Halliday. Avant de disparaître, celui-ci a décidé de léguer son immense fortune à quiconque découvrira l\'œuf de Pâques numérique qu\'il a pris soin de dissimuler dans l\'OASIS. L\'appât du gain provoque une compétition planétaire. Mais lorsqu\'un jeune garçon, Wade Watts, qui n\'a pourtant pas le profil d\'un héros, décide de participer à la chasse au trésor, il est plongé dans un monde parallèle à la fois mystérieux et inquiétant…', ''),
+(8, 'Le Monde de Nemo', 2003, '', 'Dans les eaux tropicales de la Grande Barrière de corail, un poisson‐clown du nom de Marin mène une existence paisible avec son fils unique, Nemo. Redoutant l’océan et ses risques imprévisibles, il fait de son mieux pour protéger son fils. Comme tous les petits poissons de son âge, celui‐ci rêve pourtant d’explorer les mystérieux récifs. Lorsque Nemo disparaît, Marin devient malgré lui le héros d’une quête unique et palpitante. Le pauvre papa ignore que son rejeton à écailles a été emmené jusque dans l’aquarium d’un dentiste. Marin ne s’engagera pas seul dans l’aventure : la jolie Dory, un poisson‐chirurgien bleu à la mémoire défaillante et au grand cœur, va se révéler d’une aide précieuse. Les deux poissons vont affronter d’innombrables dangers, mais l’optimisme de Dory va pousser Marin à surmonter toutes ses peurs.', ''),
+(9, 'Joker', 2019, '', 'Dans les années 1980, à Gotham City, Arthur Fleck, un humoriste de stand‐up raté, bascule dans la folie et devient le Joker.', ''),
+(10, 'À deux mètres de toi', 2019, '', 'Stella Grant, dix-sept ans, est atteinte par la fibrose kystique. Elle a passé une grande partie de sa vie entre les quatres murs de sa chambre d\'hôpital. C\'est dans ce contexte, qu\'elle tombe amoureuse de Will Newman, également atteint de la mucoviscidose. Leurs états de santé respectifs les empêchent de s\'approcher trop près l\'un de l\'autre. De plus, Stella doit tenter de raisonner son amoureux qui ne veut plus prendre ses traitements médicaux.', ''),
+(11, 'Toy Story', 1995, '', 'Quand le jeune Andy quitte sa chambre, ses jouets se mettent à mener leur propre vie sous la houlette de son pantin préféré, Woody le cow‐boy. Andy ignore également que chaque anniversaire est une source d’angoisse pour ses jouets qui paniquent à l’idée d’être supplantés par un nouveau venu. Ce qui arrive quand Buzz l’éclair est offert à Andy. Cet intrépide aventurier de l’espace, venu d’une lointaine galaxie, va semer la zizanie dans ce petit monde et vivre avec Woody d’innombrables aventures aussi dangereuses que palpitantes.', ''),
+(12, 'Spider-Man 2', 2004, '', '2 ans après avoir choisi sa vie de super-héros, Peter Parker ne parvient plus à gérer sa double vie. Il perd son boulot, Mary-Jane sait qu\'elle ne peut plus compter sur lui, et ses études prennent le même chemin. Il décide de raccrocher le costume de Spiderman. Son ami Harry lui présente un ami de son professeur de science, le bienveillant docteur Otto Octavius sur lequel il doit rédiger un mémoire. Celui-ci travaille sur la fusion qu\'il croit pouvoir contrôler. Mais une démonstration de ses recherches tourne mal, et le docteur se voit doter de 4 membres mécaniques supplémentaires greffés sur sa colonne vertébrale et qui prennent le contrôle de son esprit...', ''),
+(13, 'The Hate U Give', 2018, '', 'Starr est témoin de la mort de son meilleur ami d’enfance, Khalil, tué par balles par un officier de police. Confrontée aux nombreuses pressions de sa communauté, Starr doit trouver sa voix et se battre pour ce qui est juste.', ''),
+(14, 'The Dark Knight', 2008, '', 'La suite de Batman Begins, The Dark Knight, le réalisateur Christopher Nolan et l\'acteur Christian Bale, qui endosse à nouveau le rôle de Batman/Bruce Wayne dans sa guerre permanente contre le crime. Avec l\'aide du Lieutenant de Police Jim Gordon et du Procureur Harvey Dent, Batman entreprend de démanteler définitivement les organisations criminelles de Gotham. L\'association s\'avère efficace, mais le trio se heurte bientôt à un nouveau génie du crime, plus connu sous le nom du Joker, qui va plonger Gotham dans l\'anarchie et pousser Batman à la limite entre héros et assassin. Heath Ledger interprète Le Joker : le méchant suprême et Aaron Eckhart joue le rôle de Dent. Maggie Gyllenhaal complète le casting en tant que Rachel Dawes. De retour après Batman Begins, Gary Oldman est à nouveau Gordon, Michael Caine interprète Alfred, et Morgan Freeman est Lucius Fox.', ''),
+(15, 'LEGO Batman : Le film', 2017, '', 'Il en rêvait depuis La Grande Aventure LEGO : Batman est enfin le héros de son propre film ! Mais la situation a bien changé à Gotham – et s\'il veut sauver la ville des griffes du Joker, il lui faudra arrêter de jouer au justicier masqué et découvrir le travail d\'équipe ! Peut-être pourra-t-il alors se décoincer un peu…', ''),
+(16, 'Malcolm & Marie', 2021, '', 'Après la projection en avant-première de son dernier film, un cinéaste rentre chez lui avec sa petite amie. Alors qu\'il est certain que son film rencontrera un succès critique et commercial, la soirée prend une tournure inattendue : les deux amoureux doivent affronter certaines vérités sur leur couple qui mettent à l\'épreuve la force de leurs sentiments…', ''),
+(17, 'Palm Springs', 2020, '', 'L\'insouciant Nyles rencontre la demoiselle d', ''),
+(18, 'À la recherche du bonheur', 2006, '', 'Représentant de commerce, Chris Gardner a du mal à gagner sa vie. Il jongle pour s\'en sortir, mais sa compagne supporte de moins en moins leur précarité. Elle finit par quitter Chris et leur petit garçon de cinq ans, Christopher. Désormais seul responsable de son fils, Chris se démène pour décrocher un job, sans succès. Lorsqu\'il obtient finalement un stage dans une prestigieuse firme de courtage, il se donne à fond, même si pour le moment il n\'est pas payé. Incapable de régler son loyer, il se retrouve à la rue avec Christopher. Le père et le fils dorment dans des foyers ou des gares, cherchant des refuges de fortune au jour le jour. Perdu dans la pire épreuve de sa vie, Chris continue à veiller sur Christopher, puisant dans l\'affection et la confiance de son fils la force de surmonter les obstacles...', ''),
+(19, 'Zack Snyder\'s Justice League', 2021, '', 'Bruce Wayne est déterminé à faire en sorte que le sacrifice ultime de Superman ne soit pas vain. Pour cela, avec l\'aide de Diana Prince, il met en place un plan pour recruter une équipe de métahumains afin de protéger le monde d\'une menace apocalyptique imminente. La tâche s\'avère plus difficile qu\'il ne l\'imaginait, car chacune des recrues doit faire face aux démons de son passé, et les surpasser, pour se rassembler et former une ligue de héros sans précédent. Désormais unis, Batman, Wonder Woman, Aquaman, Cyborg et Flash réussiront-ils à sauver la planète de Steppenwolf, DeSaad, Darkseid et de leurs terribles intentions ?', ''),
+(20, 'Cherry', 2021, '', 'Un aide-soignant militaire est de retour de la guerre en Irak. Atteint de stress post-traumatique, il se met à braquer des banques pour financer sa dépendance aux opiacés.', ''),
+(21, 'The Truman Show', 1998, '', 'Truman Burbank mène une vie calme et heureuse. Il habite dans un petit pavillon propret de la radieuse station balnéaire de Seahaven. Il part tous les matins à son bureau d’agent d’assurances dont il ressort huit heures plus tard pour regagner son foyer, savourer le confort de son habitat modèle, la bonne humeur inaltérable et le sourire mécanique de sa femme, Meryl. Mais parfois, Truman étouffe sous tant de bonheur et la nuit l’angoisse le submerge. Il se sent de plus en plus étranger, comme si son entourage jouait un rôle. Pis encore, il se sent observé.', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id_film`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `film`
+--
+ALTER TABLE `film`
+  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
